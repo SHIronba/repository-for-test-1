a034a64905332deba0d2412690128b01083add1c
SHIronba <3069612966@qq.com>
2026-07-17 09:18:21 +0800

    似乎已安装了所有依赖

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import'/src/assets/less/index.less'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app =createApp(App)
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router).mount('#app')
